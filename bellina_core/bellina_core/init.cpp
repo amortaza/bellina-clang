7dcbc36bef8f9d47f85de19036464a0e058718e5
amortaza <davidnia1@gmail.com>
2016-04-03 16:17:55 -0700

    initial
    blank screen comes up

---
+++ bellina_core/bellina_core/init.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+
+#include "g2.h"
+
+g2::TextureRef* ref;
+
+void Init_OnGL() {
+	g2::init();
+
+	ref = g2::loadTextureRgb("c:\\_c\\g2\\a.jpg");
+}
+
+void UnInit() {
+	delete ref;
+
+	g2::uninit();
+}
---
+++ bellina_core/bellina_core/main.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+
+#include "Main.h"
+
+void OnTick() {
+	try
+	{
+		Draw();
+	}
+	catch (char* msg) {
+		printf(msg);
+		printf("\n");
+	}
+
+	Xel::Swap();
+}
+
+int main(int argc, char** argv) {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	Xel::Init();
+
+	Xel::Window::SetCallbacks(OnResize, Init_OnGL, UnInit);
+
+	Xel::Mouse::SetCallbacks(
+		OnMouseMove,
+		OnMouseButton,
+		OnMouseScroll);
+
+	Xel::Keyboard::SetCallbacks(
+		OnKeyDown,
+		OnKeyUp);
+
+	Xel::Window::SetTitle("Bellina v0.1.0");
+	Xel::Window::SetPosition(200, 100);
+	Xel::Window::SetSize(1280, 800);
+
+	Xel::Loop(OnTick);
+
+	printf("good bye.\n");	return 0;
+}
+
