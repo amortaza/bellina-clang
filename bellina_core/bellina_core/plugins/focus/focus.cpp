6fcca57ae73f94a984c05ada22398c68ba5b7ec4
amortaza <davidnia1@gmail.com>
2016-04-11 22:49:14 -0700

    kinda works

--- bellina_core/bellina_core/plugins/focus/focus.cpp
+++ bellina_core/bellina_core/plugins/focus/focus.cpp
@@ -1,17 +1,33 @@
 #include "stdafx.h"
 
+#include "bellina/bl-extern.h"
+#include "bellina/bl-core.h"
+
 #include "focus.h"
 
-namespace focus {
-	void init() {
+using namespace bl;
 
+namespace focus {
+	char* focusNodeId = 0;
+
+	void init() {
 	}
 
 	void uninit() {
-
+		if (focusNodeId) delete[] focusNodeId;
 	}
 
-	void tick(bl::Node *node) {
+	void tick(Node *node) {
+		bl::color(200, 255, 35);
 
+		bl::onMouseDown([](Xel::Mouse::Button button, int mx, int my, Node* bubbledFrom) {
+			focusNodeId = _strdup(bl::node->nid);
+		});
+
+		bl::onKeyDown([](unsigned long long xcode, Node* bubbledFrom) {
+			if (focusNodeId && strcmp(focusNodeId, bl::node->nid) == 0) {
+				printf("Receiveing key %s\n", bl::node->nid);
+			}
+		});
 	}
 }
